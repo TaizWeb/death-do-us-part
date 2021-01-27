71261228676a05a70d288ea3cb3655c6f2cf4548
taiz <walter.kerly@protonmail.com>
2021-01-27 14:54:26 -0500

    Add Jail and Bridge

--- src/tiles.lua
+++ src/tiles.lua
@@ -7,3 +7,24 @@ Ground = {
 	scaleY = 25/16,
 	isSolid = false
 }
+
+Bridge = {
+	id = "bridge",
+	texture = love.graphics.newImage("assets/bridge.png"),
+	width = 25,
+	height = 25, 
+	scaleX = 25/16,
+	scaleY = 25/16,
+	isSolid = false
+}
+
+Jail = {
+	id = "jail",
+	texture = love.graphics.newImage("assets/jail.png"),
+	width = 25,
+	height = 25, 
+	scaleX = 25/16,
+	scaleY = 25/16,
+	isSolid = false
+}
+
