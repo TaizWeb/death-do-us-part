e9e35460538776dad9fa1a9466fe799e5e251592
taiz <walter.kerly@protonmail.com>
2021-01-28 12:41:09 -0500

    Add walls to the game

--- src/tiles.lua
+++ src/tiles.lua
@@ -2,7 +2,7 @@ Ground = {
 	id = "ground",
 	texture = love.graphics.newImage("assets/ground.png"),
 	width = 25,
-	height = 25, 
+	height = 25,
 	scaleX = 25/16,
 	scaleY = 25/16,
 	isSolid = false
@@ -12,7 +12,7 @@ Bridge = {
 	id = "bridge",
 	texture = love.graphics.newImage("assets/bridge.png"),
 	width = 25,
-	height = 25, 
+	height = 25,
 	scaleX = 25/16,
 	scaleY = 25/16,
 	isSolid = false
@@ -22,9 +22,62 @@ Jail = {
 	id = "jail",
 	texture = love.graphics.newImage("assets/jail.png"),
 	width = 25,
-	height = 25, 
+	height = 25,
 	scaleX = 25/16,
 	scaleY = 25/16,
-	isSolid = false
+	isSolid = true
+}
+
+-- ok forgive me for this, but heartbeat didn't have tile rotation and I didn't want to change the entire engine's level formats for a jam so I committed this sin
+WallRight = {
+	id = "wallRight",
+	texture = love.graphics.newImage("assets/brick.png"),
+	width = 50,
+	height = 50,
+	scaleX = 50/32,
+	scaleY = 50/32,
+	isSolid = true
+}
+
+WallLeft= {
+	id = "wallLeft",
+	texture = love.graphics.newImage("assets/brick.png"),
+	width = 50,
+	height = 50,
+	scaleX = -50/32,
+	scaleY = 50/32,
+	isSolid = true
+}
+
+WallUp = {
+	id = "wallUp",
+	texture = love.graphics.newImage("assets/brick.png"),
+	width = 50,
+	height = 50,
+	scaleX = 50/32,
+	scaleY = -50/32,
+	isSolid = true,
+	rotation = math.pi/2
+}
+
+WallDown = {
+	id = "wallDown",
+	texture = love.graphics.newImage("assets/brick.png"),
+	width = 50,
+	height = 50,
+	scaleX = 50/32,
+	scaleY = 50/32,
+	isSolid = true,
+	rotation = math.pi/2
+}
+
+Corner = {
+	id = "corner",
+	texture = love.graphics.newImage("assets/corner.png"),
+	width = 50,
+	height = 50,
+	scaleX = 50/32,
+	scaleY = 50/32,
+	isSolid = true
 }
 
